e701116c3b7e23d7a50449a763eeaf4ce354cbba
AlexMinez <alex_minihan@outlook.com>
2022-03-11 13:23:30 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+
+let driver = {
+    driverName: 'greg',
+    driverAddress: '123 fake street'
+}
+
+
+function updateDriverWithKeyAndValue(driver,driverName,driverAddress){
+    let newDriver = {...driver};
+    newDriver[driverName] = driverAddress
+    return newDriver
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, driverName, driverAddress){
+    driver[driverName]=driverAddress
+    return driver
+}
+function deleteFromDriverByKey(driver){
+    const aDriver = {...driver}
+    delete aDriver.driverName
+    return aDriver.driverName = {driverName: 'sam'}
+}
+function destructivelyDeleteFromDriverByKey(driver,driverName){
+ Reflect.deleteProperty(driver,driverName)
+ return driver
+}
